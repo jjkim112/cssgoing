c8251c910459a533c3aa3adf46469af4f0843a57
NangmanCo <admin@nangman.io>
2023-07-11 15:17:14 +0900

    배포 설정

--- src/compounds/ProjectTicketCheckCompound.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import ProjectTicketSlider from '@/components/Project/home/ProjectTicketSlider';
-function ProjectTicketCheckCompound() {
-  return <ProjectTicketSlider available={true} />;
-}
-
-export default ProjectTicketCheckCompound;
